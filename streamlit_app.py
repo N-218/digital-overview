7cb65b72785d0a2a420a1cd4b10be4c3642a17ef
N-218 <71188910+N-218@users.noreply.github.com>
2025-11-05 23:34:49 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,7 @@
+uploaded_file = st.sidebar.file_uploader("Upload Digital_Oversight_Forecast.csv", type=["csv"])
+if uploaded_file:
+    df = pd.read_csv(uploaded_file)
+else:
+    st.warning("Please upload your CSV in the sidebar to see the dashboard.")
+    st.stop()
 
